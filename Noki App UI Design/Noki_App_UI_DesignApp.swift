6de7256f53704097af51b2459a4e9e26cf5e3d3f
Surendra-iOS <surendra.patel@thinkhat.ai>
2024-07-18 16:04:46 +0530

    Initial Commit

---
+++ Noki App UI Design/Noki_App_UI_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Noki_App_UI_DesignApp.swift
+//  Noki App UI Design
+//
+//  Created by surendra on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Noki_App_UI_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Noki App UI DesignUITests/Noki_App_UI_DesignUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Noki_App_UI_DesignUITestsLaunchTests.swift
+//  Noki App UI DesignUITests
+//
+//  Created by surendra on 18/07/24.
+//
+
+import XCTest
+
+final class Noki_App_UI_DesignUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
