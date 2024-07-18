5f93fc884777935ed63b74004b78d2e5d3d2fd66
Surendra-iOS <surendra.patel@thinkhat.ai>
2024-07-18 16:52:13 +0530

    Login Screen Updated

---
+++ Noki App UI Design/THLogin.swift
@@ -0,0 +1,18 @@
+//
+//  THLogin.swift
+//  Noki App UI Design
+//
+//  Created by surendra on 18/07/24.
+//
+
+import SwiftUI
+
+struct THLogin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    THLogin()
+}
